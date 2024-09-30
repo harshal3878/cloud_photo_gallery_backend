a9df362dced2dd0178cc12f75297af814b19e1a9
harshal3878 <harshalghatolgit@gmail.com>
2024-09-30 13:05:09 +0530

    implemented browse images functionality.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from cloud_photo_gallery_backend import create_app
+
+#export PYTHONPATH=/home/harshal/learn   -use this to setup python module path 
 app = create_app()
 if __name__ == '__main__':
     app.run(debug=True)
--- routes.py
+++ routes.py
@@ -4,6 +4,7 @@ from botocore.exceptions import NoCredentialsError, ClientError
 import random
 import string
 
+
 main = Blueprint('main', __name__)
 
 # AWS S3 configuration
@@ -33,19 +34,31 @@ def get_image():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-    
+
+
+
+
+#get grid of images stored for user.    
 @main.route('/browse')
 def get_images():
     try:
-        # Generate a presigned URL for the requested image
-        presigned_url = s3_client.generate_presigned_url('get_object',
+        initial_image_list = s3_client.list_objects(
+        Bucket=S3_BUCKET,
+        MaxKeys=20,
+        Prefix=S3_FOLDER)
+        image_keys = []
+        image_presigned_urls = []
+        for image in initial_image_list["Contents"]:
+            image_key = image["Key"] 
+            print(image_key)
+            if image_key != S3_FOLDER:
+                image_keys.append(image_key)
+                presigned_url = s3_client.generate_presigned_url('get_object',
                                                          Params={'Bucket': S3_BUCKET,
-                                                                 'Key': f'{S3_FOLDER}{filename}'},
-                                                         ExpiresIn=360)  
-        
-        print("presigned_url: "+presigned_url)
-        # Redirect to the presigned S3 URL
-        return presigned_url
+                                                                 'Key': image_key },
+                                                         ExpiresIn=360)
+                image_presigned_urls.append(presigned_url)
+        return jsonify({'success': True, 'message': image_presigned_urls}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -56,12 +69,14 @@ def get_images():
 def upload_image():
     try:
 
-        # generate random name and Upload to S3
+        # generate random name and Upload to S3        
         characters = string.ascii_letters + string.digits
         random_string = ''.join(random.choice(characters) for _ in range(10))
         file = request.files['image']
+        image_extension = str(file.filename).split(".")[1]
+        print(image_extension)
         print(f"uploading image to :{S3_FOLDER}{random_string}")
-        s3_client.upload_fileobj(file, S3_BUCKET, f"{S3_FOLDER}{random_string}")
+        s3_client.upload_fileobj(file, S3_BUCKET, f"{S3_FOLDER}{random_string}.{image_extension}")
         
     except NoCredentialsError:
         print(jsonify({'success': False, 'error': 'Credentials not available'}))
